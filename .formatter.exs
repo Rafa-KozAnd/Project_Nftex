3e6be980fadc8c4da28396322098f4766ed5dad7
Rafael Kozlowski Andreola <rafandreola@hotmail.com>
2023-04-30 19:53:46 -0300

    Create .formatter.exs

---
+++ .formatter.exs
@@ -0,0 +1,5 @@
+[
+  import_deps: [:ecto, :ecto_sql, :phoenix, :absinthe],
+  subdirectories: ["priv/*/migrations"],
+  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}", "priv/*/seeds.exs"]
+]
